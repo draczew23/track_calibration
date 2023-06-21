ee1ed73fe64dbc098a8e2e9b81e8457209d9e75a
Jakub Wiechnik <kuba.wiechnik23@gmail.com>
2023-06-21 16:24:15 +0200

    basic file loading works

--- track_check.py
+++ track_check.py
@@ -11,9 +11,9 @@ my_place = os.getcwd()  # find my path location
 os.chdir(my_place)  # change the path to the working directory
 
 our_files = []  # list of our data files
-files_list = os.listdir(my_place) # list of our data files)
+files_list = os.listdir(my_place)  # list of our data files)
 
-for file in files_list: # creating list of files
+for file in files_list:  # creating list of files
     file_path = os.path.join("", file)  # construct the full file path
     if os.path.isfile(file_path) and file.startswith("ppg"):
         our_files.append(file)
@@ -22,13 +22,24 @@ for file in files_list: # creating list of files
 bxsize = 1.
 bxmin = -550.
 bxmax = 550
-bysize = 1e-2 
+bysize = 1e-2
 bymin = 0
 bymax = 1.
 ### Histogram parameters ###
 
-if our_files:
+if our_files:   # check if we have an empty list
     print("Non empty")
+    for datafile in our_files:
+        print("----------------------")
+        print("Loading file: " + datafile)
+        print("----------------------")
+        with uproot.open(datafile) as file:
+
+            file_keys = file.keys()
+            key_to_find = "STENTU/h1"
+            print(file["STENTU/h1"])
+
+
 else:
     print("empty file list")
 
