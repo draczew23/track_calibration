54e86d9386ac735b124ef6f657b0c466cf5c88ca
Jakub Wiechnik <kuba.wiechnik23@gmail.com>
2023-07-08 15:08:32 +0200

    All basic plots are correct and workin; badrun implemented

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
 import ROOT
 import numpy as np
 import matplotlib.pyplot as plt
+from BadRun import IsBadRun
+from ROOT import gROOT
 
 myFile = ROOT.TFile.Open("ppgphok5_stentu_pcadtm_all.root")
 ROOT.ROOT.EnableImplicitMT()
+gROOT.SetStyle("Bold")
 
 ourTree = myFile.STENTU
 h1 = ourTree.h1
-branch = h1.GetBranch("pxtr")
-print(branch)
-nentries = branch.GetEntries()
+branch_pxtr = h1.GetBranch("pxtr")
+branch_pytr = h1.GetBranch("pytr")
+branch_pztr = h1.GetBranch("pztr")
+branch_Runnumber = h1.GetBranch("Runnumber")
+
+BadRun_checker = IsBadRun()     # BadRun initialization
+
+nentries = branch_pxtr.GetEntries()
 print(nentries)
 
 ### Histogram parameters ###
@@ -21,16 +29,30 @@ bymin = 0
 bymax = 1.
 ### Histogram parameters ###
 
-how_many = 3
+how_many = 3    # number of values per leaf
 
+# Creation of (3, nentries) arrays
+# Columns: pxtr, pxtr1, pxtr2
+ 
 pxtr = np.empty((0, how_many))
 pytr = np.empty((0, how_many))
 pztr = np.empty((0, how_many))
 
 scaledown = 500000
 nn = int(nentries/scaledown)
+
+# Test histogram
+hist = ROOT.TH1F("hist", "Histogram", int((bxmax-bxmin)/bxsize), int(bxmin), int(bxmax))
+
 for i in range(nn):
-    branch.GetEntry(i)
+    branch_pxtr.GetEntry(i)
+    branch_pytr.GetEntry(i)
+    branch_pztr.GetEntry(i)
+    branch_Runnumber.GetEntry(i)
+    runnumber = np.append(runnumber, branch_Runnumber.GetLeaf("Runnumber").GetValue(i))
+
+    if BadRun_checker.is_bad_run(runnumber):
+        continue
 
     temp_x = np.array([])
     temp_y = np.array([])
@@ -38,9 +60,11 @@ for i in range(nn):
 
     for j in range(how_many):
 
-        temp_x = np.append(temp_x, branch.GetLeaf("pxtr").GetValue(j))
-        temp_y = np.append(temp_y, branch.GetLeaf("pxtr").GetValue(j))
-        temp_z = np.append(temp_z, branch.GetLeaf("pxtr").GetValue(j))
+        if (branch_pxtr.GetLeaf("pxtr").GetValue(j) * branch_pytr.GetLeaf("pytr").GetValue(j) * branch_pztr.GetLeaf("pztr").GetValue(j) != 0):
+            hist.Fill(branch_pztr.GetLeaf("pztr").GetValue(j))
+        temp_x = np.append(temp_x, branch_pxtr.GetLeaf("pxtr").GetValue(j))
+        temp_y = np.append(temp_y, branch_pytr.GetLeaf("pytr").GetValue(j))
+        temp_z = np.append(temp_z, branch_pztr.GetLeaf("pztr").GetValue(j))
 
     pxtr = np.vstack((pxtr, temp_x))
     pytr = np.vstack((pytr, temp_y))
@@ -50,6 +74,33 @@ filter_ar = pxtr*pytr*pztr
 filter_ar_condition = filter_ar != 0
 
 k = np.array(pxtr[filter_ar_condition])
+l = np.array(pytr[filter_ar_condition])
+m = np.array(pztr[filter_ar_condition])
 
-plt.hist(k, bins=int((bxmax-bxmin)/bxsize))
-plt.savefig("example.png")
+plt.hist(k, bins=int((bxmax-bxmin)/bxsize), range=(int(bxmin), int(bxmax)), edgecolor='red', linewidth=1.5)
+plt.savefig("pxtr_0.png")
+plt.clf()
+
+plt.hist(l, bins=int((bxmax-bxmin)/bxsize), range=(int(bxmin), int(bxmax)))
+plt.savefig("pytr_0.png")
+
+plt.clf()
+plt.hist(m, bins=int((bxmax-bxmin)/bxsize), range=(int(bxmin), int(bxmax)))
+plt.savefig("pztr_0.png")
+
+canvas = ROOT.TCanvas("canvas", "Histogram Canvas", 800, 600)
+hist.Draw()
+canvas.Update()
+canvas.SaveAs("test_histogram.png")
+
+# plt.hist(k1, bins=int((bxmax-bxmin)/bxsize))
+# plt.hist(k1[:, 0], bins=int((bxmax-bxmin)/bxsize))
+# plt.clf()
+
+# plt.hist(l, bins=int((bxmax-bxmin)/bxsize))
+# plt.savefig("example1.png")
+# plt.clf()
+
+# plt.hist(m, bins=int((bxmax-bxmin)/bxsize))
+# plt.savefig("example2.png")
+# plt.clf()
